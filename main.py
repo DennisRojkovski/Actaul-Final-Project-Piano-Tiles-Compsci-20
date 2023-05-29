4e00d1b2775516adfdaff4ddc79d1d9384b71e16
DennisRojkovski <133244991+DennisRojkovski@users.noreply.github.com>
2023-05-29 13:43:31 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ screen_height = 800
 screen = pygame.display.set_mode((screen_width, screen_height))
 mixer.music.set_volume(1)
 pygame.mixer.set_num_channels(13)
-font = pygame.font.SysFont('timesnewroman',  30)
+keys_font = pygame.font.SysFont('Verdana',  30)
+message_font = pygame.font.SysFont('Verdana', 20)
 c1_key = pygame.Rect(8, 600, 88, 300)
 d_key = pygame.Rect(108, 600, 88, 300)
 e_key = pygame.Rect(208, 600, 88, 300)
@@ -24,35 +25,32 @@ fs_key = pygame.Rect(370, 600, 70, 150)
 gs_key = pygame.Rect(470, 600, 70, 150)
 as_key = pygame.Rect(570, 600, 70, 150)
 keys_rects = [c1_key, cs_key, d_key, ds_key, e_key, f_key, fs_key, g_key, gs_key, a_key, as_key, b_key, c2_key]
-keys = [pygame.K_a, pygame.K_w,
-pygame.K_s,
-pygame.K_e,
-pygame.K_d,
-pygame.K_f,
-pygame.K_t,
-pygame.K_g,
-pygame.K_y,
-pygame.K_h,
-pygame.K_u,
-pygame.K_j,
-pygame.K_k,
-]
+keys = [pygame.K_a, pygame.K_w, pygame.K_s, pygame.K_e, pygame.K_d, pygame.K_f, pygame.K_t, pygame.K_g, pygame.K_y, pygame.K_h, pygame.K_u, pygame.K_j, pygame.K_k]
 notes_list = ['C1', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B', 'C2']
 sounds = ['c', 'c-', 'd', 'd-', 'e', 'f', 'f-', 'g', 'g-', 'a', 'a-', 'b', 'c']
 keys_dict = {}
 for i in range(len(keys)):
   keys_dict[keys[i]] = {'rect': keys_rects[i],'note': notes_list[i],'pressed': False,'channel': i,'sound': sounds[i]}
 
+with open("highscore.txt", 'r+') as highscore_file:
+  highscore = highscore_file.read()
+  print(highscore, 'highscore after')
 
 def get_valid_num(question, range):
   asking = True
   while asking:
     try:
       answer = int(input(question))
-      if answer < 1 or answer > range:
-        print("Answer not in proper range.")
+      if range != None:
+        if answer < 1 or answer > range:
+          print("Answer not in proper range.")
+        else:
+          asking = False
       else:
-        asking = False
+        if answer < 1:
+          print("Answer not in proper range.")
+        else:
+          asking = False
     except:
       print("Not an integer answer.")
   return answer
@@ -61,18 +59,20 @@ def get_valid_num(question, range):
 playing = True
 def play_game(playing):
   while playing:
-      which_mode = get_valid_num("Do you wish to play endless mode or learn a new song? type '1' for endless, '2' for new song.", 2)
+      which_mode = get_valid_num("Do you wish to play endless mode or learn a new song? type '1' for endless, '2' for new song or 3 to quit: ", 3)
       if which_mode == 1:
-        playing = play_song('endless')
+        chosen_lives = get_valid_num("How many lives do you wish to have? (min 1, max 10): ", 10)
+        chosen_max = get_valid_num("What score do you wish to be the winning score? (10 is a little, 50 is a good amount, 100+ is insane): ", None)
+        playing = play_song('endless', chosen_lives = chosen_lives, chosen_max = chosen_max)
       elif which_mode == 2:
         playing = choose_preset_song()
-
-
+      else:
+        playing = False
 
 def get_random_key():
   x_list = [8, 108, 208, 308, 408, 508, 608, 708]
   x_sharp_list = [70, 170, 370, 470, 570]
-  which_list = random.randint(1,2)
+  which_list = random.randint(1, 2)
   if which_list == 2:
     x = random.choice(x_sharp_list)
     width = 35
@@ -89,15 +89,6 @@ def get_random_key():
     b = 227
   return x, width, height, r, g, b
 
-
-
-
-
-
-
-
-
-
 win_img = pygame.image.load("win_screen.png")
 win_img = pygame.transform.scale(win_img,(screen_width, screen_height))
 lose_img = pygame.image.load("lose_screen.png")
@@ -105,20 +96,28 @@ lose_img = pygame.transform.scale(lose_img,(screen_width, screen_height))
 learn_img = pygame.image.load("learn.png")
 learn_img = pygame.transform.scale(learn_img,(screen_width, screen_height))
 
-white=(255, 255, 255)
-yellow=(255, 255, 0)
-green=(0, 255, 255)
-orange=(255, 100, 0)
+black = (0,0,0)
+white = (255, 255, 255)
 
-letter_1 = font.render("A", False, orange, yellow)
-letter_2 = font.render("S", False, orange, green)
-letter_3 = font.render("D", False, orange, yellow)
-letter_4 = font.render("F", False, orange, green)
-letter_5 = font.render("G", False, orange, yellow)
-letter_6 = font.render("H", False, orange, green)
-letter_7 = font.render("J", False, orange, yellow)
-letter_8 = font.render("K", False, orange, yellow)
+letter_1 = keys_font.render("A", False, black, white)
+letter_2 = keys_font.render("S", False, black, white)
+letter_3 = keys_font.render("D", False, black, white)
+letter_4 = keys_font.render("F", False, black, white)
+letter_5 = keys_font.render("G", False, black, white)
+letter_6 = keys_font.render("H", False, black, white)
+letter_7 = keys_font.render("J", False, black, white)
+letter_8 = keys_font.render("K", False, black, white)
+letter_9 = keys_font.render("W", False, white, black)
+letter_10 = keys_font.render("E", False, white, black)
+letter_11 = keys_font.render("T", False, white, black)
+letter_12 = keys_font.render("Y", False, white, black)
+letter_13 = keys_font.render("U", False, white, black)
+start_message = message_font.render("Press the right key to begin.", False, black)
+record_message = message_font.render(f"All-time record: {highscore}", False, black)
+
+
 key_names = [letter_1, letter_2, letter_3, letter_4, letter_5, letter_6, letter_7, letter_8]
+sharp_names = [letter_9, letter_10, letter_11, letter_12, letter_13]
 
 
 
@@ -160,21 +159,16 @@ def choose_preset_song():
     playing = make_preset_song('mii_channel')
 
 
-
-
-
-
-
-
-
-
-def play_song(mode, song_notes = '', note_speed = 0):
+def play_song(mode, song_notes = '', note_speed = 0, chosen_lives = 10, chosen_max = None):
+  
+  with open("highscore.txt") as highscore_file:
+    highscore = int(highscore_file.read())
   if mode == 'endless':
     x, width, height, r, g, b = get_random_key()
     score = 0
-    tiles_fallen = 0
     velocity_increase = 0
-    lives = 5
+    lives = chosen_lives
+  tiles_fallen = 0
   hit = False
   mistake = 0
   y = 200
@@ -184,21 +178,28 @@ def play_song(mode, song_notes = '', note_speed = 0):
   octave = 4
   playing_notes = True
   key_is_falling = True
-
   while playing_notes:
     while key_is_falling:
+      record_message = message_font.render(f"All-time record: {highscore}", False, black)
+      mistake_message = message_font.render(f"{mistake} mistake(s)", False, black)
+      score_message = message_font.render(f"score: {score}", False, black)
       if mode == 'endless':
-        if score == 10 or lives == 0:
-          if score == 10:
-            game_verdict = 'win'
-          elif lives == 0:
-            game_verdict = 'lose'
+        if lives > 1:
+          lives_message = message_font.render(f"{lives} lives", False, black)
+        else:
+          lives_message = message_font.render("1 life", False, black)
+        if score == chosen_max:
+          game_verdict = 'win'
+          key_is_falling = False
+          playing_notes = False
+        elif lives == 0:
+          game_verdict = 'lose'
           key_is_falling = False
           playing_notes = False
 
 
         if (tiles_fallen % 5) == 0 and tiles_fallen != 0:
-          velocity_increase += 1.5
+          velocity_increase += 1.25
           velocity += velocity_increase
           tiles_fallen += 1
 
@@ -210,11 +211,15 @@ def play_song(mode, song_notes = '', note_speed = 0):
           x, width, height, r, g, b = get_random_key()
           y = 100
         else:
-          screen.fill((189, 151, 30))
-          pygame.time.delay(100)
-          y += velocity
+          if tiles_fallen == 0:
+            screen.fill((189, 151, 30))
+            screen.blit(start_message, (20,10))
+            y = y
+          else:
+            screen.fill((189, 151, 30))
+            pygame.time.delay(100)
+            y += velocity
           pygame.draw.rect(screen, (r, g, b), (x, y, width, height))
-
       else:
         if y >= 430:
           y = 100
@@ -228,11 +233,15 @@ def play_song(mode, song_notes = '', note_speed = 0):
           x = song_notes[note_count]
           screen.fill((189, 151, 30))
           pygame.time.delay(100)
-          y += note_speed
-          pygame.draw.rect(screen, (255,255,255), (x, y, 60, 140))
-      
-        
-
+          if tiles_fallen == 0:
+            screen.blit(start_message, (20,10))
+            y = y
+          else:
+            y += note_speed
+          if x != 70 and x != 170 and x != 370 and x != 470 and x != 570:
+            pygame.draw.rect(screen, (255,255,255), (x, y, 88, 200))
+          else:
+            pygame.draw.rect(screen, (0,0,0), (x, y, 35, 200))
 
       j = -1
       for key, value in keys_dict.items():
@@ -247,11 +256,27 @@ def play_song(mode, song_notes = '', note_speed = 0):
             pygame.draw.rect(screen, (255,255,255), rectangle)
           else:
             pygame.draw.rect(screen, (0,0,0), rectangle)
-        key_location = 50
-        for key_name in key_names:
-          
-          screen.blit(key_name, (key_location, 750))
-          key_location += 80
+        key_location = 40
+      for key_name in key_names:
+        screen.blit(key_name, (key_location, 760))
+        key_location += 100
+      key_location = 75
+      key_counter = 0
+      for key_name in sharp_names:
+        screen.blit(key_name, (key_location, 700))
+        key_counter += 1
+        if key_counter == 2:
+          key_location += 202
+        else:
+          key_location += 101
+      if mode == 'endless':
+        screen.blit(mistake_message, (20, 35))
+        screen.blit(lives_message, (20, 60))
+        screen.blit(score_message, (20, 85))
+        if score > highscore:
+          highscore = score
+        screen.blit(record_message, (550, 10))
+        
       pygame.display.flip()
 
       if x == 8:
@@ -306,8 +331,6 @@ def play_song(mode, song_notes = '', note_speed = 0):
           if event.key in keys_dict:
             keys_dict[event.key]['pressed'] = False
 
-
-
       if hit:
           if mode == 'endless':
             score += 1
@@ -319,8 +342,9 @@ def play_song(mode, song_notes = '', note_speed = 0):
           else:
             y = 100
             note_count += 1
-
+            tiles_fallen += 1
           hit = False
+
       if mode == 'endless':
         if mistake == 3:
             print("3 mistakes. Lost a life.")
@@ -341,7 +365,8 @@ def play_song(mode, song_notes = '', note_speed = 0):
       screen.blit(learn_img, (0, 0))
       pygame.display.flip()
       play_again_loop = True
-
+  with open('highscore.txt', 'w') as highscore_file:
+    highscore_file.write(str(highscore))
   while play_again_loop:
     play_again = input("Do you want to play again? ('y' or 'n'): ")
     if play_again == 'y':
